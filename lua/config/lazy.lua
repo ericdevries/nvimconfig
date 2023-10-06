6ad537ca53a02daee74c1c2ac0033e370d91fbbf
Eric de Vries <hro.mail@gmail.com>
2023-10-06 07:22:47 +0200

    add lua line

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup({
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
     -- version = false, -- always use the latest git commit
-    version = "*",  -- try installing the latest stable version for plugins that support semver
+    version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
@@ -45,3 +45,4 @@ require('config.options')
 
 vim.cmd [[colorscheme tokyonight]]
 
+require('lualine').setup()
